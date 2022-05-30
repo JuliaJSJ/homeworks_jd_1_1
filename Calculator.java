baca3ec29653248f1bdaee7c30a02f3576685396
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-05-31 01:25:24 +0300

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+import java.util.function.*;
+
+@FunctionalInterface
+interface Supplier<T> {
+    T get();
+}
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y==0) {
+            System.out.println("Деление на ноль");
+            return 0;
+        } else
+        return x / y;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
